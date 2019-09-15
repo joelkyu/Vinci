94868d428a9e415118fa63a12927825ee6a6da31
Tayef Shah <tayefshah@gmail.com>
2019-09-15 02:40:00 -0400

    its fixed, almost

--- transaction_parse.js
+++ transaction_parse.js
@@ -1,32 +1,33 @@
 const cust_json = require('./1000customers.json');
 const fs = require('fs');
+const axios = require('axios');
 
-//POST Request to get transaction data filtered (1000 entries)
-var myTransaction = {
-    method: 'POST',
-    headers: {
-        'Authorization': 'YOUR API KEY GOES HERE'
-    }, 
-    body: {
-        "searchCriteria": [     //Filter by time of transaction
-          {
-            "key": "originationDateTime",
-            "operation": ":",
-            "value": "2019-09-16"
-          }
-        ]
-      }
-};
+const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiOTgzMWU3M2UtZDIzNi0zOGE2LWI1MDctZmEwOWVkN2RjOWY5IiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI5M2MwZDZiNi0xYzk3LTRkNzgtYjE4Ny0wYWJjMmQyOTExY2MifQ.RgZi5yONFvsEnJZIBvtE4wkNDiPQ2RhGmWnRqbJffpk"
 
-//New request from 
-var myRequest;
 
-// fetch(myRequest)
-//   .then(response => response.json())
-//   .then(json => {
-//     // the json variable contains the response from the API
-//     console.log(json); 
-//   });
+function getTransactions(customer_id) {
+  axios.post('https://api.td-davinci.com/api/simulants/' + customer_id + '/simulatedtransactions/search', {
+    header: {
+      Authorization: API_KEY,
+    },
+    requestBody: {
+      "searchCriteria": [     //Filter by time of transaction
+        {
+          "key": "originationDateTime",
+          "operation": ":",
+          "value": "2019-09-12"
+        }
+      ]
+    }
+  })
+  .then((res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+}
 
 //Point to the array of customers
 var customers_json = cust_json;
@@ -38,18 +39,17 @@ var transactions = {};
 transactions.list = [];
 
 
-for(var i = 0; i < 1000; i++){
-    customer_id = customer_list[i].id;
-    // myRequest = new Request('https://api.td-davinci.com/api/simulants/' + customer_id + '/simulatedtransactions/search', myTransaction);
-    // fetch(myRequest);
-    transactions.list[i] = 'yee'
+for (var i = 0; i < 1000; i++) {
+  customer_id = customer_list[i].id;
+  transactions.list[i] = getTransactions(customer_id);
 }
 
-var transaction_content = JSON.stringify(transactions)
+var transaction_content = JSON.stringify(transactions);
+
 fs.writeFile('transactions.json', transaction_content, "utf8", function (err) {
   if (err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
+    console.log("An error occured while writing JSON Object to File.");
+    return console.log(err);
   }
 
   console.log("JSON file has been saved.");
