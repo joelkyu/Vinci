ad1af7fe68314d92168e16f198bf2325f4ad115a
Tayef Shah <tayefshah@v1040-wn-rt-a-120-131.campus-dynamic.uwaterloo.ca>
2019-09-15 01:18:42 -0400

    add comments

--- transaction_parse.js
+++ transaction_parse.js
@@ -1,4 +1,4 @@
-import cust_json from '1000customers.json';
+import json from '1000customers.json';
 const fs = require('fs');
 
 //POST Request to get transaction data filtered (1000 entries)
@@ -11,8 +11,8 @@ var myTransaction = {
         "searchCriteria": [     //Filter by time of transaction
           {
             "key": "originationDateTime",
-            "operation": ">",
-            "value": "2019-09-12"
+            "operation": ":",
+            "value": "2019-09-16"
           }
         ]
       }
@@ -29,22 +29,22 @@ fetch(myRequest)
   });
 
 //Point to the array of customers
-var customers_json = JSON.parse(cust_json);
+
 var customer_list = customers_json.results.customers;
 var customer_id;
 
 //transactions object
-var transactions = {};
-transactions.list = [];
+// var transactions = {};
+// transactions.list = [];
 
 
-for(var i = 0; i < 1000; i++){
-    customer_id = customer_list[i].id;
-    myRequest = new Request('https://api.td-davinci.com/api/simulants/' + customer_id + '/simulatedtransactions/search', myTransaction);
-    fetch(myRequest);
-    transactions.list[i] = myRequest
-}
+// for(var i = 0; i < 1000; i++){
+//     customer_id = customer_list[i].id;
+//     myRequest = new Request('https://api.td-davinci.com/api/simulants/' + customer_id + '/simulatedtransactions/search', myTransaction);
+//     fetch(myRequest);
+//     transactions.list[i] = myRequest
+// }
 
-var transaction_content = JSON.stringify(transactions)
+// var transaction_content = JSON.stringify(transactions)
 
-fs.writeFile('transactions.json', transaction_content, "utf8");
+// fs.writeFile('transactions.json', transaction_content, "utf8");
